49db864c9bc20e600cecef23469806834505810e
kelseybcrow <kelseybcrow@gmail.com>
2022-04-08 10:42:56 -0600

    second commit

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
+import Home from './Home';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     // BEM naming convention
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search"
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+// import { Link } from "react-router-dom";
 
 
 function Header() {
@@ -11,22 +13,29 @@ function Header() {
 
             <div className="header__search">
                 <input className='header__searchInput' type='text' />
-                {/* SEARCH ICON BROKE THE PAGE */}
                 {/* <SearchIcon className="header__searchIcon" /> */}
             </div>
             <div className="header__nav">
                 <div className="header__option">
                     <span className='header__optionLineOne'>Hello Guest</span>
-                    <span className='header__optionLineOne'>Sign In</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
                 </div>
                 <div className="header__option">
                     <span className='header__optionLineOne'>Returns</span>
-                    <span className='header__optionLineOne'>Orders</span>
+                    <span className='header__optionLineTwo'>Orders</span>
                 </div>
                 <div className="header__option">
                     <span className='header__optionLineOne'>Your</span>
-                    <span className='header__optionLineOne'>Prime</span>              
+                    <span className='header__optionLineTwo'>Prime</span>              
                 </div>
+                {/* <Link to="/checkout"> */}
+                    <div className="header__optionBasket">
+                        {/* <ShoppingBasketIcon /> */}
+                        <span className="header__optionLineTwo header__basketCount">
+                    {/* {basket?.length} */}
+                        </span>
+                    </div>
+                {/* </Link> */}
             </div>
         </div>
     )

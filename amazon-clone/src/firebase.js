6007fe0c6c366324d2491c3e5076061f8024eecd
kelseybcrow <kelseybcrow@gmail.com>
2022-04-08 10:42:38 -0600

    first commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQtjP0nA4cn6pJ_tfbSTRPyzM4sIRZ7Zc",
+  authDomain: "clone-1ed38.firebaseapp.com",
+  projectId: "clone-1ed38",
+  storageBucket: "clone-1ed38.appspot.com",
+  messagingSenderId: "96022206891",
+  appId: "1:96022206891:web:102a5c0406267393501aee"
+};
